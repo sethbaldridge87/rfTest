40bb1acbe2fd78eb98f259f9332273ac456e7893
sethbaldridge87 <sethbaldridge87@gmail.com>
2019-06-26 22:51:51 -0600

    Table added

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 $(document).ready(function(){
-    // var allison = {
-    //     "First Name":"Allison",
-    //     "Preferred First Name for Badge":"Al",
-    //     "Last Name":"Profitt",
-    //     "Company":"Bio-IT World",
-    //     "Onsite - Badge Holders":"JVA",
-    //     "Onsite-Aux":"1",
-    //     "Check-in":"09/16/16 7:51 AM"
-    // }
-    // var schedule = {
-    //     "07:00 AM":"Find Sessions",
-    //     "08:00 AM":"BR1058 Exchange Search and knowledge discovery until Storage Technologies 08:00 AM - 08:00 AM - 08:45 AM",
-    //     "09:00 AM":"Find Sessions",
-    //     "10:00 AM":"BR1063 Write Data virtualization by Hadoop",
-    //     "11:00 AM":"Find Sessions"
-    // }
-    // var badge;
-    // for (var i in allison) {
-    //     console.log(allison[i]);
-    //     var newRow = '<div><p><b>' + [i] + ':</b> ' + allison[i] + '</p></div>'
-    //     $('#badgeOutput').append(newRow);
-    // }
+    $('#mobile_nav img').click(function(){
+        if ($('#sideNav').hasClass('mobile')) {
+            $('#sideNav').animate({left:'-60px'});
+            $('#sideNav').removeClass('mobile');
+        } else {
+            $('#sideNav').animate({left:'0'});
+            $('#sideNav').addClass('mobile');
+        }
+    });
+    $(window).resize(function(){
+        $('#sideNav').removeClass('mobile');
+        if($(window).width() >= 535){
+            $('#sideNav').css('left','0');
+        } else {
+            $('#sideNav').css('left','-60px');
+        }
+    })
+    
 });
